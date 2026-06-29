9dc719312b1cf9a40d62d41a13fc41b2b59757ce
brijeshearn12-dotcom <brijeshearn12@gmail.com>
2026-06-30 00:44:50 +0530

    Add Maharashtra districts and soil type constants

---
+++ lib/constants.ts
@@ -0,0 +1,8 @@
+export const SOIL_TYPES = [
+  { id: 'sandy',        label: 'Sandy',        icon: '🏖️' },
+  { id: 'loamy',        label: 'Loamy',        icon: '🌱' },
+  { id: 'clayey',       label: 'Clayey',       icon: '🧱' },
+  { id: 'black_cotton', label: 'Black Cotton', icon: '🌾' },
+] as const
+
+export type SoilTypeId = (typeof SOIL_TYPES)[number]['id']
